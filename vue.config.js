852988dbea6b7044db38bc418b294619f676f3eb
Ozan <okoybas35@gmail.com>
2025-06-18 17:45:16 +0200

    delete

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/roofing-company-website/' : '/',
-  lintOnSave: false,
-  configureWebpack: {
-    devtool: 'source-map'
-  }
-}
